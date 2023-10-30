2781b8bdcaa6d4ccb114d65c8fe057a246127d2b
Parissa <zahra.pmf@gmail.com>
2023-10-30 20:35:56 +0100

    all functions are set

--- school.py
+++ school.py
@@ -1,7 +1,7 @@
 # pylint:disable=C0111,C0103
 import sqlite3
 conn = sqlite3.connect('data/school.sqlite')
-db = conn.cursor()
+dbb = conn.cursor()
 
 def students_from_city(db, city):
     """return a list of students from a specific city"""
